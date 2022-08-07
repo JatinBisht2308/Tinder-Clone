bf7f74becfdae2fc7fe453ebc18f4d47d44bc9bd
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-08-07 17:03:42 +0530

    :fire: created the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
 import PersonIcon from "@mui/icons-material/Person";
 import ForumIcon from "@mui/icons-material/Forum";
+import {IconButton} from "@mui/material";
 import {SiTinder} from "react-icons/si";
 const Header = () => {
   return (
     <div className='header' >
+      <IconButton>
       <PersonIcon className="header-icons" fontSize="large" />
-      <SiTinder />
+      </IconButton>
+      <SiTinder className="tinder-logo" />
+      <IconButton>
       <ForumIcon className="header-icons" fontSize="large" />
+      </IconButton>
     </div>
   )
 }
