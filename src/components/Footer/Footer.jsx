f7d79f724133b5079afcc296d7f08c3d61694f29
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-08-16 11:40:37 +0530

    :fire: created the footer and added the swiping functionality

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import "./Footer.css";
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from "@mui/icons-material/Close";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+import IconButton from '@mui/material/IconButton';
 const Footer = () => {
   return (
-    <div>
-      <h2>Hi i am a footer.</h2>
+    <div className="footer" >
+      <IconButton  >
+        <ReplayIcon className="footer-icons replay"  fontSize="large"/>
+      </IconButton>
+      <IconButton>
+        <CloseIcon className="footer-icons close" fontSize="large"/>
+      </IconButton>
+      <IconButton>
+        <StarRateIcon className="footer-icons star" fontSize="large"/>
+      </IconButton>
+      <IconButton>
+        <FavoriteIcon className="footer-icons fav" fontSize="large"/>
+      </IconButton>
+      <IconButton>
+        <FlashOnIcon className="footer-icons flash" fontSize="large"/>
+      </IconButton>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/TinderCards/TinderCards.jsx
+++ src/components/TinderCards/TinderCards.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
-
+import './TinderCard.css'
+import TinderCard from 'react-tinder-card';
+import { cardClasses } from '@mui/material';
 const TinderCards = () => {
+  // using hooks(hooks is nothing but just a function)
+  const [bikes, setBikes] = React.useState([
+    // creating the list of the bikes
+    {
+      name:"BMW s1000",
+      url:"https://indwallpapers.com/wp-content/uploads/2021/07/BMW-S1000RR-Android-Wallpaper.jpeg"
+    },
+    {
+      name:"Ducati Panigale",
+      url:"https://w0.peakpx.com/wallpaper/699/721/HD-wallpaper-basil-ducati-v4s-beast-bike-panigale-red-sportsbike.jpg"
+    },
+    {
+      name:"Suzuki Hayabhusa",
+      url:"https://i.pinimg.com/564x/85/53/37/8553374b4495e0908f6bc9424c68f0c7.jpg"
+    },
+    {
+      name:"Kawasaki Z900",
+      url:"https://cutewallpaper.org/22/kawasaki-z400-wallpapers/450321896.jpg"
+    },
+    {
+      name:"Honda CBR1000RR",
+      url:"https://w0.peakpx.com/wallpaper/301/849/HD-wallpaper-cbr1000rr-colours-motorcycle.jpg"
+    },
+    {
+    name:"Yamaha R1",
+    url:"https://w0.peakpx.com/wallpaper/628/97/HD-wallpaper-yamaha-r1-bike-motorcycle-sports.jpg"
+    },
+    {
+    name:"Suzuki GSXR1000",
+    url:"https://w0.peakpx.com/wallpaper/255/979/HD-wallpaper-suzuki-gsx-r-bike-doctor-motorcycle.jpg"
+    },
+    {
+    name:"KTM Duke 1290",
+    url:"https://pictures.topspeed.com/IMG/crop_webp/202206/1290super-duke-r-evo-109-2_1600x0.webp"
+    }
+  ]);
+      
   return (
-    <div>
-      <h1>Hi i am having the cards.</h1>
+    <div className='tinder-cards'>
+     {
+        bikes.map(bike => (
+          <TinderCard className="swipe"
+          key={bike.name}
+          preventSwipe={['up','down']}
+          >
+            <div className='card'  
+            style={{backgroundImage: `url(${bike.url})`}}
+            >
+             <h3>{bike.name}</h3>
+            </div>
+          </TinderCard>
+        ))
+     }
     </div>
   )
 }
